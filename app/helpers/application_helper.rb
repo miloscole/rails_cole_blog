7d8a7036a9e87827cb08527fa24710ace89447d3
miloscole <27miloscole@gmail.com>
2025-02-08 15:53:20 +0100

    Add article CRUD, add stylesheet toolkit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def custom_line_break(num = 5)
+    ("<br>" * num).html_safe
+  end
+
+  def page_title(text)
+    content_tag(:article, class: "container") do
+      content_tag(:h4, text, class: "padding-x-medium margin-0")
+    end
+  end
 end
